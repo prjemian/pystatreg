873fc419f0f7fc3b593e26cb9eefe78d8ba57659
Pete R Jemian <prjemian@gmail.com>
2023-01-01 16:26:22 -0600

    DOC #4 discard unused import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,7 +3,6 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-from importlib.metadata import version
 import configparser
 import pathlib
 import sys
