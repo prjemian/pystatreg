71135ebeae734def5ad99bb76757cb18d4610e23
Pete R Jemian <prjemian@gmail.com>
2023-01-01 14:26:30 -0600

    CI #4 switch to pydata-sphinx-theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,6 +53,6 @@ today_fmt = "%Y-%m-%d %H:%M"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-# html_theme = "pydata_sphinx_theme"
 html_static_path = ["_static"]
+html_theme = "pydata_sphinx_theme"
+html_title = "PySumReg"
